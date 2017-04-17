7768175153bc59adf5c1c38be5f4ad52f8c760b8
Anton Anikeev <000job00@gmail.com>
2017-04-17 20:11:18 +0300

    Base setup & add-config tests

---
+++ AppForStudy/Tests/BasicTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class BasicTest
+    {
+        [Fact]
+        public void Add()
+        {
+            var test = 1;
+            var test2 = 1 + 2;
+            test2.Should().Be(3);
+        }
+    }
+}
